7974e54027fbfe83af23a01b797a9d942b276bdf
manash <pratimkashyapm@gmail.com>
2026-03-10 15:08:11 +0530

    creating webserver using node,js

--- webServer.js
+++ webServer.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const server = http.createServer((request, response) => {
+    response.end('Hello from the server')
     console.log("A new request received")
+    // console.log(response)
 })
 
 server.listen(8080, '127.0.0.1' ,()=>console.log("sever is running on port 8080"))
